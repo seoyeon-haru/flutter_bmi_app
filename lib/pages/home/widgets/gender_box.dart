466ae3a05560a07c4e573b401291d85b1d3534c0
seoyeon-haru <sy5792@naver.com>
2025-07-12 19:38:23 +0900

    Create new file

---
+++ lib/pages/home/widgets/gender_box.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GenderBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('GenderBox');
+  }
+
+}
---
+++ lib/pages/home/widgets/slider_box.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SliderBox extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Text('SliderBox');
+  }
+
+}
