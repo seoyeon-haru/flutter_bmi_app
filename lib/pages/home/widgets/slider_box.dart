67f87aeaa357149b116403613340da5c66ae9e39
seoyeon-haru <sy5792@naver.com>
2025-07-12 22:52:58 +0900

    Implement Row

--- lib/pages/home/widgets/slider_box.dart
+++ lib/pages/home/widgets/slider_box.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
 
-class SliderBox extends StatelessWidget{
+class SliderBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(),
-        Slider(value: 170, onChanged: (v) {}, min: 1, max: 300,)
+        Row(
+          children: [
+            Text('HEIGHT'),
+            Spacer(),
+            Text('170'),
+            Text('cm'),
+          ],
+        ),
+        Slider(
+          value: 170,
+          onChanged: (v) {},
+          min: 1,
+          max: 300,
+        )
       ],
     );
   }
-
 }
